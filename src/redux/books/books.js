dc083463ba028dc34d6a5465841265678b9ccebe
Benjamin Semah <benjaminsemah@gmail.com>
2022-03-02 13:13:35 +0000

    Fix errors spotted by linters

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -13,12 +13,12 @@ export const removeBook = (id) => ({
   id,
 });
 
-const reducer = (state = initialState, { type, payload, id }) => {
-  switch (type) {
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
     case ADD_BOOK:
-      return [...state, payload];
+      return [...state, action.payload];
     case REMOVE_BOOK:
-      return state.filter((book) => book.id !== id);
+      return state.filter((book) => book.id !== action.id);
     default:
       return state;
   }
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,4 +1,8 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import {
+  createStore,
+  combineReducers,
+  applyMiddleware,
+} from 'redux';
 import logger from 'redux-logger';
 import booksReducer from './books/books';
 
