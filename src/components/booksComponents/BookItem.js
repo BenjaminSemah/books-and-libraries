1ac7eeae468861a8171558e65d6470e437fcd3cc
Benjamin Semah <benjaminsemah@gmail.com>
2022-03-03 10:29:08 +0000

    Add create/remove book functionality via API

--- src/components/booksComponents/BookItem.js
+++ src/components/booksComponents/BookItem.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { removeBook } from '../../redux/books/books';
+import { loadBooks, removeBook } from '../../redux/books/books';
 
 const BookItem = () => {
-  const bookState = useSelector((state) => state.booksReducer);
   const dispatch = useDispatch();
+  const bookState = useSelector((state) => state.booksReducer);
+
+  useEffect(() => {
+    dispatch(loadBooks());
+  }, []);
+
   const removeHandler = (e) => {
     dispatch(removeBook(e.target.id));
   };
@@ -13,10 +18,10 @@ const BookItem = () => {
     return (
       <>
         {bookState.map((book) => (
-          <div key={book.id} className="book--card">
+          <div key={book.item_id} className="book--card">
             <h2 className="book--title">{book.title}</h2>
-            <h3 className="book--author">{book.author}</h3>
-            <button className="remove--btn" type="button" id={book.id} onClick={removeHandler}>
+            <p className="benji--test">Benji test</p>
+            <button className="remove--btn" type="button" id={book.item_id} onClick={removeHandler}>
               Remove
             </button>
           </div>
